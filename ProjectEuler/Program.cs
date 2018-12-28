d1242255b1a87eaba23dc62483eeccc849f50458
Goombario <12705535+Goombario@users.noreply.github.com>
2018-12-28 14:02:50 -0500

    Initial commit

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public abstract class Question
+    {
+        public abstract bool Run();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var question1 = new Question1();
+            question1.Run();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjectEuler/Question1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Question1 : Question
+    {
+        public override bool Run()
+        {
+            int sum = Attempt1();
+            Console.WriteLine($"The total sum was {sum}");
+            return true;
+        }
+
+        public int Attempt1()
+        {
+            // Process 1: Use sets to get all the unique multiples
+            HashSet<int> uniqueMultiples = new HashSet<int>();
+
+            FindAndAddMultiplesToSet(3, 1000, uniqueMultiples);
+            FindAndAddMultiplesToSet(5, 1000, uniqueMultiples);
+
+            Console.WriteLine($"There are {uniqueMultiples.Count} unique multiples in this set.");
+
+            return uniqueMultiples.Sum();
+        }
+
+        private void FindAndAddMultiplesToSet(int naturalNumberToMultiply, int maxExclusive, HashSet<int> set)
+        {
+            // Error checking to make sure the number is natural
+            if (naturalNumberToMultiply <= 0) return;
+
+            for (var i = naturalNumberToMultiply; i < maxExclusive; i += naturalNumberToMultiply)
+            {
+                set.Add(i);
+            }
+        }
+    }
+}
